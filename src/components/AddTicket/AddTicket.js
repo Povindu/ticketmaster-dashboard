26351255f52fd878ab7666283d8fde855e4d98c1
Povindu Samarasekara <povinduchanmith@gmail.com>
2023-12-05 20:39:48 +0530

    Ticket Adding feature added

--- src/components/AddTicket/AddTicket.js
+++ src/components/AddTicket/AddTicket.js
@@ -1,4 +1,9 @@
 import React from 'react';
+
+import {db} from '../../config/config';
+import {ref, set, onValue, get} from "firebase/database";
+import { useEffect, useState } from 'react';
+
 import './AddTicketStyle.css';
 
 const AddTicket = () => {
@@ -11,12 +16,17 @@ const AddTicket = () => {
 
         if(ticketNo == '' || name == '' || mobile == ''){
             alert('Please fill all the fields');
-            return;
         }
         else{
+            const reference = ref(db, 'tickets/' + ticketNo);
             
-
-
+            set(reference, {
+                id:ticketNo,
+                name: name,
+                mobile: mobile,
+                pass: pass,
+                checkIn : false
+            })
 
             alert('Ticket Added');
             document.getElementsByClassName('inp-TicketNo')[0].value = '';
@@ -25,17 +35,19 @@ const AddTicket = () => {
             document.getElementsByClassName('inp-pass')[0].value = '';
         }
 
-    }
 
+
+        
+        }
 
   return (
+
     <div>
         <input className="inp-TicketNo" placeholder='Ticket No' type='text'/>
         <input className="inp-name" placeholder='Name' type='text'/>
         <input className="inp-mobile" placeholder='Mobile Number'type='text'/>
         <input className="inp-pass" placeholder='Pass'type='text'/>
         <button className="btn-add" onClick={createTicket}>Add Ticket</button>
-
     </div>
   );
 };
